7c99e17223eea6d95d2bcbe3385d494c9ddd20e7
xiewei12 <51935689+xiewei12@users.noreply.github.com>
2019-06-18 09:38:58 +0800

    谢威还原了方法

--- Educational_Management_System/src/main/java/com/newer/edu/stu/service/impl/InstituteServiceImpl.java
+++ Educational_Management_System/src/main/java/com/newer/edu/stu/service/impl/InstituteServiceImpl.java
@@ -4,7 +4,5 @@ import com.newer.edu.stu.service.InstituteService;
 
 public class InstituteServiceImpl implements InstituteService {
 
-	public void function() {
-		System.out.println(111111);
-	}
+
 }
